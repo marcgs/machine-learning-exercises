8b9df1db4f9ac3002869f509eeaec3ebf0a21b3a
Marc Gomez <marc.gomez82@gmail.com>
2020-04-27 18:12:46 -0500

    add solution to first problem

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -9,8 +9,9 @@ function [J, grad] = lrCostFunction(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+h = sigmoid(X*theta);
+J = 1/m * (-y'*log(h) - (1-y)'*log(1-h)) + lambda/(2*m) * sum(theta(2:end).^2);
+grad = 1/m * X'*(h-y) + lambda/m * [0; theta(2:end,1)];
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
