aa5198847d6a46b75fbaf2deb86283f2db91e123
Marc Gomez <marc.gomez82@gmail.com>
2020-05-03 17:33:10 +0200

    Neural network prediction

--- ex3/predict.m
+++ ex3/predict.m
@@ -21,11 +21,12 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+a1 = [ones(m, 1) X];
+z2 = a1*Theta1';
+a2 = [ones(m, 1) sigmoid(z2)];
+a3 = sigmoid(a2 * Theta2'); 
 
-
-
-
-
+[M, p] = max(a3, [], 2);
 
 
 
